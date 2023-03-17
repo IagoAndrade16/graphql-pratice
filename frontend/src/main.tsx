325133d2d8ead5af0d4a873ee221642f271fb115
Iago Alexandre <iagoaap@outlook.com>
2023-03-17 06:04:44 -0300

    finished pratice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
+import { ApolloProvider } from '@apollo/client/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { client } from './lib/apollo'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
